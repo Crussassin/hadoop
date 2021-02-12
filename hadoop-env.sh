4ac8de01b305ecebc48095cd021ea69b556407c6
Crussassin <Slavaalekseenk@gmail.com>
2021-02-12 22:32:33 +0300

    Update hadoop-env.sh

--- hadoop-env.sh
+++ hadoop-env.sh
@@ -1,6 +1,6 @@
 #
 
-export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64/jre"
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64/jre/"
 
 export HADOOP_HOME=/usr/local/hadoop/current
 
