9336ef8f945929f953924bae419fdaff9b0a034a
Crussassin <Slavaalekseenk@gmail.com>
2021-02-13 01:07:18 +0300

    Update hadoop-env.sh

--- hadoop-env.sh
+++ hadoop-env.sh
@@ -1,8 +1,4 @@
-
-
 export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64/jre"
-
 export HADOOP_HOME=/usr/local/hadoop/current
-
 export HADOOP_HEAPSIZE_MAX=512m
 export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
